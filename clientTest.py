6eef1f0a49f002aa8644400c880539d94173a13d
Rachel Schwall <rachel92124@gmail.com>
2021-10-15 13:32:32 -0700

    fixed ip address for client

--- clientTest.py
+++ clientTest.py
@@ -1,6 +1,6 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('8.8.8.8', 80))
+client.connect(('169.254.58.251', 8081))
 client.send("I am CLIENT\n".encode())
 from_server = client.recv(4096)
 client.close()
