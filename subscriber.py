6bcc5230e58eb813f080d3f816231dd8236802b1
Rachel Schwall <rachel92124@gmail.com>
2021-10-16 13:12:26 -0700

    added more comms screenshots

--- subscriber.py
+++ subscriber.py
@@ -16,7 +16,7 @@ def on_disconnect(client, userdata, rc):
 # The default message callback.
 # (you can create separate callbacks per subscribed topic)
 def on_message(client, userdata, message):
-    print('Received message: "' + str(message.payload) + '" on topic "' +message.topic + '" with QoS ' + str(message.qos))
+    print('Received message: "' + str(message.payload.decode()) + '" on topic "' +message.topic + '" with QoS ' + str(message.qos))
 
 # 1. create a client instance.
 client = mqtt.Client()
