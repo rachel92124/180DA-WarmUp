3b97e9ea9bd07ab1315b5bf7c442d076998edc3e
Rachel Schwall <rachel92124@gmail.com>
2021-10-21 14:19:21 -0700

    IMU screenshots, speech recognition

---
+++ lab4/speech_rec.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+print(sr.__version__)
+r = sr.Recognizer()
+
+harvard = sr.AudioFile('audio_files_harvard.wav')
+with harvard as source:
+    audio = r.record(source)
+
+print(type(audio))
+print(r.recognize_google(audio))
